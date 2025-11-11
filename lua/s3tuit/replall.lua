5f265bdd28e42322c204f4a16281341ff4c2d068
Matteo Ladislai <matteo.ladislai@hotmail.com>
2025-11-11 20:18:00 +0100

    Refactor Replall function

--- lua/s3tuit/replall.lua
+++ lua/s3tuit/replall.lua
@@ -1,40 +1,47 @@
-local function escape_pat(s, delim)
-  s = s:gsub("\\", "\\\\")
-  s = s:gsub(vim.pesc(delim), "\\" .. delim)
-  return s
-end
+--[[
+This is a helper function that, given two arguments, performs a conditional
+replace for the whole project of the old pattern (first argument) with
+the new pattern (last argument). The function does not perform auto
+escaping. The delimiter used depends on the input, if '/' is not present
+in the inputs, it gets used, else '#'.
 
-local function escape_repl(s, delim)
-  s = s:gsub("\\", "\\\\")
-  s = s:gsub("&", "\\&")
-  s = s:gsub(vim.pesc(delim), "\\" .. delim)
-  return s
-end
+Example:
+
+:Replall \(vim\|vi\) miv
+
+expands to:
+
+:silent vimgrep /\(vim\|vi\)/ **/*
+:cfdo %s/\(vim\|vi\)/miv/sgc | update
+--]]
 
 vim.api.nvim_create_user_command("Replall", function(opts)
   if #opts.fargs ~= 2 then
-    vim.notify("Usage: :GrepAll {old_text} {new_text}", vim.log.levels.ERROR)
+    vim.notify("Usage: :Replall {old_pattern} {new_pattern}", vim.log.levels.ERROR)
     return
   end
 
-  old = opts.fargs[1]
+  local old = opts.fargs[1]
   local new = opts.fargs[2]
 
+  -- Choose a delimiter that’s not in the patterns (no automatic escaping)
   local delim = (old:find("/", 1, true) or new:find("/", 1, true)) and "#" or "/"
 
-  -- Fill quickfix using current :grepprg
-  vim.cmd("silent grep " .. vim.fn.shellescape(old))
+  -- 1) Fill quickfix with all matches in project using vimgrep
+  -- You are responsible for escaping {old} as a Vim regex.
+  local vimgrep_cmd = string.format("silent vimgrep %s%s%s **/*", delim, old, delim)
+  vim.cmd(vimgrep_cmd)
 
   if vim.tbl_isempty(vim.fn.getqflist()) then
-    vim.notify("No matches for: " .. old, vim.log.levels.INFO)
+    vim.notify("No matches for pattern: " .. old, vim.log.levels.INFO)
     return
   end
 
-  local pat  = escape_pat(old, delim)
-  local repl = escape_repl(new, delim)
-  local cmd = string.format("cfdo %%s%s\\V%s%s%s%sgc | update", delim, pat, delim, repl, delim)
-  vim.cmd(cmd)
+  -- 2) Apply substitution in all files from quickfix
+  -- Again: {old} and {new} are used as-is, no \V, no extra escaping.
+  local sub_cmd = string.format("cfdo %%s%s%s%s%s%sgc | update", delim, old, delim, new, delim)
+  vim.cmd(sub_cmd)
 end, {
   nargs = "+",           -- allow one or more; enforced exactly 2 above
-  complete = nil,       
-})
+  complete = nil,
+  })
